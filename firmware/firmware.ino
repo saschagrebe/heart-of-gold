cfeafbb9248ba0d70f13ef64366799b71f74ac74
Sascha Grebe <saschagre@gmail.com>
2016-11-11 22:33:47 +0100

    make firmware compatible with java client

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,24 +1,25 @@
+#include <SPI.h>
 #include <UIPEthernet.h>
 
 #include <Adafruit_DotStar.h>
 
 #define LOGO_NUMPIXELS         4
-#define STATUS_RING_NUMPIXELS  30
+#define STATUS_RING_NUMPIXELS  15
 
 // Here's how to control the LEDs from any two pins:
 #define LOGO_DATAPIN         5
 #define LOWER_RING_DATAPIN   6
 #define UPPER_RING_DATAPIN   7
 #define CLOCKPIN             8
-Adafruit_DotStar logo = Adafruit_DotStar(LOGO_NUMPIXELS, LOGO_DATAPIN, CLOCKPIN, DOTSTAR_BRG);
-Adafruit_DotStar lowerRing = Adafruit_DotStar(STATUS_RING_NUMPIXELS, LOWER_RING_DATAPIN, CLOCKPIN, DOTSTAR_BRG);
-Adafruit_DotStar upperRing = Adafruit_DotStar(STATUS_RING_NUMPIXELS, UPPER_RING_DATAPIN, CLOCKPIN, DOTSTAR_BRG);
-uint32_t lowerRingColors[30] = {};
-uint32_t upperRingColors[30] = {};
+Adafruit_DotStar logo = Adafruit_DotStar(LOGO_NUMPIXELS, LOGO_DATAPIN, CLOCKPIN, DOTSTAR_RGB);
+Adafruit_DotStar lowerRing = Adafruit_DotStar(STATUS_RING_NUMPIXELS, LOWER_RING_DATAPIN, CLOCKPIN, DOTSTAR_RGB);
+Adafruit_DotStar upperRing = Adafruit_DotStar(STATUS_RING_NUMPIXELS, UPPER_RING_DATAPIN, CLOCKPIN, DOTSTAR_RGB);
+uint32_t lowerRingColors[15] = {};
+uint32_t upperRingColors[15] = {};
 
 // networking stuff
 EthernetServer server = EthernetServer(5999);
-uint8_t mac[6] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
+uint8_t mac[6] = {0x02, 0x3C, 0xBB, 0xF0, 0x0E, 0xB7};
 
 void setup() {
   Serial.begin(9600);
@@ -31,22 +32,30 @@ void setupLeds() {
   logo.begin();
   lowerRing.begin();
   upperRing.begin();
+
+  // init logo
+  logo.setPixelColor(0, 0, 100, 255);
+  logo.setPixelColor(1, 125, 255, 0);
+  logo.setPixelColor(2, 0, 255, 0);
+  logo.setPixelColor(3, 255, 0, 255);
+  logo.setBrightness(255);
+  logo.show();
   
   // Turn all LEDs off ASAP
-  logo.show();
   lowerRing.show();
   upperRing.show();
 }
 
 void setupNetwork() {
   // use DHCP for gathering an IP address
-  Ethernet.begin(mac);
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed DHCP");
+  }
   Serial.println(Ethernet.localIP());
   
   server.begin();
 }
 
-boolean connected = false;
 String cmd = "";
 EthernetClient client;
 boolean rotateLower = false;
@@ -55,18 +64,26 @@ boolean rotateUpper = false;
 void loop() {
   client = server.available();
   if (client) {
-    if (!connected) {
-      client.flush();
-      connected = true;
-    }
-
     while (client.available() > 0) {
       readTelnetCommand(client.read());
     }
-    // TODO update LED states
+    
+    updateLeds();
   }
   
-  delay(10);
+  delay(20);
+}
+
+void updateLeds() {
+  for (int i = 0; i < STATUS_RING_NUMPIXELS; i++) {
+    lowerRing.setPixelColor(i, lowerRingColors[i]);
+  }
+  lowerRing.show();
+  
+  for (int i = 0; i < STATUS_RING_NUMPIXELS; i++) {
+    upperRing.setPixelColor(i, upperRingColors[i]);
+  }
+  upperRing.show();
 }
 
 void readTelnetCommand(char c) {
@@ -82,13 +99,14 @@ void readTelnetCommand(char c) {
 
 // - Help -
 // r<u|l><0|1> : rotate <upper|lower> <0|1>
-// switch <upper|lower> <pixel> <color>
-// quit
+// s<u|l><pixel><color>
+// q
 void parseCommand() {
   Serial.println(cmd);
   if(cmd.charAt(0) == 'q') {
+      Serial.println("Bye");
+      client.flush();
       client.stop();
-      connected = false;
       
   } else if(cmd.charAt(0) == 'r' && cmd.length() == 3) {
       rotateCommand();
